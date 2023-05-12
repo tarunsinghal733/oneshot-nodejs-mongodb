c01bf8c9af9b9fed85449451a6d320d3c9e78429
tarunsinghal733 <20BCS3552@cuchd.in>
2023-05-12 13:27:51 +0530

    sasevs

--- index.js
+++ index.js
@@ -9,14 +9,17 @@ app.use(bodyParser.urlencoded({
     extended:true
 }))
 // mongodb://127.0.0.1:27017/foodie-db mongosh show dbs 
-mongoose.connect('mongodb+srv://oneshot-db:one********@oneshot-db.xjmku3w.mongodb.net/?retryWrites=true&w=majority',{
+mongoose.connect('mongodb+srv://oneshot-db:onesht@db123@oneshot-db.xjmku3w.mongodb.net/?retryWrites=true&w=majority',{
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 var db = mongoose.connection;
 db.on('error',()=>console.log("Error in Connecting to Database"));
 db.once('open',()=>console.log("Connected to Database"))
+
 app.post("/contactform",(req,res)=>{
+
+    //console.log(req.body);
     var name = req.body.name;
     var email = req.body.email;
     var msgcontent = req.body.msgcontent;
@@ -25,15 +28,17 @@ app.post("/contactform",(req,res)=>{
         "name" : name,
         "email" : email,
         "msgcontent" : msgcontent
-    }
+    };
+    //console.log(req)
+    console.log(data);
     db.collection('contact-form').insertOne(data,(err,collection)=>{
         if(err){
             throw err;
         }
         console.log("Record Inserted Successfully");
     });
-    return res.redirect('index.html')
-})
+    return res.redirect('index.html');
+});
 app.get("/",(req,res)=>{
     res.set({
         "Allow-access-Allow-Origin": '*'
