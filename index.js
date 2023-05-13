c0aba9d4ea5167c50336fa70365d75b08cdbfa40
tarunsinghal733 <tarunsinghal055@gmail.com>
2023-05-13 17:21:45 +0530

    DAGRTHY

--- index.js
+++ index.js
@@ -6,40 +6,41 @@ const app = express()
 app.use(bodyParser.json())
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({
-    extended:true
+    extended: true
 }))
-// mongodb://127.0.0.1:27017/foodie-db mongosh show dbs 
-mongoose.connect('mongodb+srv://oneshot-db:@oneshot-db.xjmku3w.mongodb.net/?retryWrites=true&w=majority',{
+//mongodb://127.0.0.1:27017/foodie-db mongosh show dbs 
+mongoose.connect('mongodb+srv://oneshot-db:oneshot-db123@oneshot-db.xjmku3w.mongodb.net/?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
-var db = mongoose.connection;
-db.on('error',()=>console.log("Error in Connecting to Database"));
-db.once('open',()=>console.log("Connected to Database"))
 
-app.post("/contactform",(req,res)=>{
+var db = mongoose.connection;
+db.on('error', () => console.log("Error in Connecting to Database"));
+db.once('open', () => console.log("Connected to Database"))
+
+app.post("/contactform", (req, res) => {
 
     //console.log(req.body);
     var name = req.body.name;
     var email = req.body.email;
     var msgcontent = req.body.msgcontent;
-    
+
     var data = {
-        "name" : name,
-        "email" : email,
-        "msgcontent" : msgcontent
+        "name": name,
+        "email": email,
+        "msgcontent": msgcontent
     };
     //console.log(req)
     console.log(data);
-    db.collection('contact-form').insertOne(data,(err,collection)=>{
-        if(err){
+    db.collection('contact-form').insertOne(data, (err, collection) => {
+        if (err) {
             throw err;
         }
         console.log("Record Inserted Successfully");
     });
     return res.redirect('index.html');
 });
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.set({
         "Allow-access-Allow-Origin": '*'
     })
